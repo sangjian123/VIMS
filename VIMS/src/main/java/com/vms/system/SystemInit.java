b2c76f437b3522c1f37316405d36c9554cc5ba99
sangjian123 <38687603+sangjian123@users.noreply.github.com>
2018-05-09 14:41:54 +0800

    Add files via upload
    system

--- VIMS/src/main/java/com/vms/system/SystemInit.java
+++ VIMS/src/main/java/com/vms/system/SystemInit.java
@@ -5,7 +5,9 @@ import java.io.InputStream;
 import java.util.Calendar;
 import java.util.Map.Entry;
 import java.util.Properties;
+
 import javax.servlet.ServletContextEvent;
+
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,53 +15,52 @@ import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import com.vms.utils.AesKeyUtils;
 import com.vms.utils.ConfigHolder;
-
+import com.vms.utils.MyJedis;
 
 public class SystemInit extends ContextLoaderListener
 {
     
-    private static final Logger LOGGER = LoggerFactory.getLogger (SystemInit.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SystemInit.class);
     
-    private static Logger sysLogger = LoggerFactory.getLogger ("sysLoger");
-    
+    private static Logger sysLogger = LoggerFactory.getLogger("sysLoger");
     
     /**
      * 空字符串
      */
     private static String BLANK = "";
     
-    
     /**
      * WebApplicationContext对象
      */
     private static WebApplicationContext ctx = null;
     
-    private static void setCtx (WebApplicationContext context)
+    private static void setCtx(WebApplicationContext context)
     {
         ctx = context;
     }
     
-    public void contextInitialized (ServletContextEvent contextEvent)
+    public void contextInitialized(ServletContextEvent contextEvent)
     {
-        LOGGER.info ("System initializing...");
-        sysLogger.info ("System initializing...");
+        LOGGER.info("System initializing...");
+        sysLogger.info("System initializing...");
         try
         {
-            super.contextInitialized (contextEvent);
-            setCtx (WebApplicationContextUtils.getRequiredWebApplicationContext (contextEvent.getServletContext ()));
+            super.contextInitialized(contextEvent);
+            setCtx(WebApplicationContextUtils.getRequiredWebApplicationContext(contextEvent.getServletContext()));
             
-            LOGGER.info ("Load spring configurations successful.");
-            sysLogger.info ("Load spring configurations successful.");
+            LOGGER.info("Load spring configurations successful.");
+            sysLogger.info("Load spring configurations successful.");
             // 初始化缓存
-            LOGGER.info ("Start to load init data...");
-            sysLogger.info ("Start to load init data...");
+            LOGGER.info("Start to load init data...");
+            sysLogger.info("Start to load init data...");
             // 初始化
-            init ();
+            init();
         }
         catch (Exception e)
         {
-            LOGGER.error ("Initialized error", e);
+            LOGGER.error("Initialized error", e);
         }
     }
     
@@ -72,19 +73,19 @@ public class SystemInit extends ContextLoaderListener
      * @author xugangfeng
      * @see [类、类#方法、类#成员]
      */
-    public static Object getBean (String name)
+    public static Object getBean(String name)
     {
-        return ctx.getBean (name);
+        return ctx.getBean(name);
     }
     
     /**
      * {@inheritDoc}
      */
-    public void contextDestroyed (ServletContextEvent contextEvent)
+    public void contextDestroyed(ServletContextEvent contextEvent)
     {
-        sysLogger.info ("System destroyed...");
-        destroy ();
-        super.contextDestroyed (contextEvent);
+        sysLogger.info("System destroyed...");
+        destroy();
+        super.contextDestroyed(contextEvent);
     }
     
     /**
@@ -93,9 +94,9 @@ public class SystemInit extends ContextLoaderListener
      * @return 当前系统的绝对路径
      * @see [类、类#方法、类#成员]
      */
-    public static String getServletContextRealPath ()
+    public static String getServletContextRealPath()
     {
-        return getWebApplicationContext ().getServletContext ().getRealPath (BLANK);
+        return getWebApplicationContext().getServletContext().getRealPath(BLANK);
     }
     
     /**
@@ -104,25 +105,45 @@ public class SystemInit extends ContextLoaderListener
      * @return WebApplicationContext - WebApplicationContext
      * @see [类、类#方法、类#成员]
      */
-    public static WebApplicationContext getWebApplicationContext ()
+    public static WebApplicationContext getWebApplicationContext()
     {
         return ctx;
     }
     
-    
     /**
      * 初始化
      */
-    private void init ()
+    private void init()
     {
         // 初始化系统配置项
         try
         {
-            getConfiguration ("dubbo-config.properties");
+            getConfiguration("dubbo-config.properties");
+            getConfiguration("configuration.properties");
+            getConfiguration("redisConfig.properties");
         }
         catch (Exception e)
         {
-            LOGGER.error ("Initialized configuration。properties Fail.", e);
+            LOGGER.error("Initialized configuration。properties Fail.", e);
+        }
+        
+        try
+        {
+            String ipAndPorts = ConfigHolder.getCfg("redisCluster");
+            String[] ipAndPortArr = ipAndPorts.split(",");
+            String requirepass = AesKeyUtils.decrypt(ConfigHolder.getCfg("REDIS_PASSWORD"), ConfigHolder.getCfg("REDIS_PWD_KEY"));
+            MyJedis.init(ipAndPortArr, requirepass);
+        }
+        catch (Exception e)
+        {
+            //redis 异常
+            LOGGER.error("Redis Error!!!", e);
+            if(e.getMessage().contains("All"))
+            {
+                //所有redis都配置错误或者redis连接不上
+                LOGGER.error("All Redis Error!!!", e);
+                //发送告警短信
+            }
         }
         
     }
@@ -131,29 +152,29 @@ public class SystemInit extends ContextLoaderListener
      * 读取系统配置文件 并缓存到系统内存中去
      * @throws IOException 
      */
-    private void getConfiguration (String fileName) throws IOException
+    private void getConfiguration(String fileName) throws IOException
     {
         InputStream inputStream = null;
         try
         {
-            Properties p = new Properties ();
-            inputStream = this.getClass ().getClassLoader ().getResourceAsStream (fileName);
-            p.load (inputStream);
-            for (Entry <Object, Object> entry : p.entrySet ())
+            Properties p = new Properties();
+            inputStream = this.getClass().getClassLoader().getResourceAsStream(fileName);
+            p.load(inputStream);
+            for(Entry<Object, Object> entry : p.entrySet())
             {
-                ConfigHolder.setCfg ((String) entry.getKey (), (String) entry.getValue ());
+                ConfigHolder.setCfg((String) entry.getKey(), (String) entry.getValue());
             }
         }
         finally
         {
-            IOUtils.closeQuietly (inputStream);
+            IOUtils.closeQuietly(inputStream);
         }
     }
     
     /**
      * 定时器销毁
      */
-    private void destroy ()
+    private void destroy()
     {
         
     }
@@ -163,13 +184,13 @@ public class SystemInit extends ContextLoaderListener
      * 
      * @return
      */
-    public long getDayBegin ()
+    public long getDayBegin()
     {
-        Calendar cal = Calendar.getInstance ();
-        cal.set (Calendar.HOUR_OF_DAY, 0);
-        cal.set (Calendar.SECOND, 0);
-        cal.set (Calendar.MINUTE, 3);
-        cal.set (Calendar.MILLISECOND, 1);
-        return cal.getTimeInMillis ();
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MINUTE, 3);
+        cal.set(Calendar.MILLISECOND, 1);
+        return cal.getTimeInMillis();
     }
 }
